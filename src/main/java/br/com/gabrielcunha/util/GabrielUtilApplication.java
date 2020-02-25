2070b6bb62ba33072f011d69779110836e00d35e
gabriel <gabrielcunhadev@gmail.com>
2020-02-25 12:48:32 -0300

    Initial commit.

---
+++ src/main/java/br/com/gabrielcunha/util/GabrielUtilApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrielcunha.util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabrielUtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GabrielUtilApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/gabrielcunha/util/log/UtilLog.java
@@ -0,0 +1,12 @@
+package br.com.gabrielcunha.util.log;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class UtilLog {
+	
+	public static Log getLog() {
+		return LogFactory.getLog(Thread.currentThread().getStackTrace()[2].getClassName());
+	}
+
+}
